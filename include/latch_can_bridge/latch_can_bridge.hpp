7cdf75d0d57d474ce7e22748a4764b486204be48
Nick DaSilva <nmdas05x@gmail.com>
2026-01-12 14:38:13 -0500

    starting lister code #1

--- include/latch_can_bridge/latch_can_bridge.hpp
+++ include/latch_can_bridge/latch_can_bridge.hpp
@@ -1,20 +1,45 @@
 #pragma once
 
+#include <linux/can.h>
+#include <net/if.h>
+#include <sys/socket.h>
+
+#include <cstdint>
 #include <memory>
 #include <rclcpp/node_options.hpp>
 #include <string>
+#include <thread>
+#include <unordered_map>
 
 #include "rclcpp/rclcpp.hpp"
-
-namespace latch_can_bridge {
+#include "std_msgs/msg/int32.hpp"
+#include "yaml-cpp/yaml.h"
 
 class LatchCanBridgeNode : public rclcpp::Node {
 public:
     explicit LatchCanBridgeNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions());
 
-    
-
 private:
-};
+    // the can interface of the linux device "can0"
+    std::string can_interface_;
 
-}  // namespace latch_can_bridge
+    // the socket for the can interface
+    int socket_;
+    struct sockaddr_can socket_addr_;
+
+    // interface request var
+    struct ifreq interface_req_;
+
+    // can listener thread
+    std::thread listener_thread_;
+
+    // can config YAML file
+    YAML::Node can_config_;
+
+    // publisher types for the different can id associations
+    std::unordered_map<uint16_t, std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Int32>>> publishers_int32_;
+
+    // function defs
+    void load_can_mappings();
+    void can_listener_loop();
+};
--- src/latch_can_bridge.cpp
+++ src/latch_can_bridge.cpp
@@ -1,15 +1,60 @@
-#include <memory>
-#include <rclcpp/node_options.hpp>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <yaml-cpp/node/node.h>
+#include <yaml-cpp/parser.h>
+
+#include <ament_index_cpp/get_package_share_directory.hpp>
 
 #include "latch_can_bridge/latch_can_bridge.hpp"
 
-namespace latch_can_bridge {
-LatchCanBridgeNode::LatchCanBridgeNode(const rclcpp::NodeOptions& options) : Node("latch_can_bridge", options) {}
-}  // namespace latch_can_bridge
+LatchCanBridgeNode::LatchCanBridgeNode(const rclcpp::NodeOptions& options) : Node("latch_can_bridge", options) {
+    socket_ = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (socket_ < 0) {
+        // TODO handle errors
+    }
+
+    // create the can config yaml node
+    auto pkg_path = ament_index_cpp::get_package_share_directory("latch_can_bridge");
+    auto config_path = pkg_path + "/config/can_mappings.yaml";
+    can_config_ = YAML::LoadFile(config_path);
+
+    // Give the request the name of the desired can interface
+    strcpy(interface_req_.ifr_name, can_config_["can_bridge"]["can_interface"].as<std::string>().c_str());
+
+    load_can_mappings();
+}
+
+void LatchCanBridgeNode::load_can_mappings() {
+    // select the mappings element from the can_bridge section of yaml
+    auto can_mappings = can_config_["can_bridge"]["id_mappings"];
+
+    // loop through the mappings
+    for (auto mapped_id : can_mappings) {
+        // get the id
+        uint16_t can_id = mapped_id["can_id"].as<uint16_t>();
+        // get the topic name
+        std::string topic = mapped_id["topic"].as<std::string>();
+        // get the type
+        std::string msg_type = mapped_id["type"].as<std::string>();
+
+        // create a publisher and map it to id
+        if (msg_type == "int32") {
+            publishers_int32_[can_id] = this->create_publisher<std_msgs::msg::Int32>(topic, 10);
+        } else {
+            // TODO implement error handleing and other publisher types
+        }
+    }
+}
+
+void LatchCanBridgeNode::can_listener_loop() {
+    struct can_frame frame;
+    auto can_bytes = read(socket_, &frame, sizeof(struct can_frame));
+}
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<latch_can_bridge::LatchCanBridgeNode>();
+    auto node = std::make_shared<LatchCanBridgeNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
