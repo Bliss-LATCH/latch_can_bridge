1c241d529df115be3bb2f95c3ebbe84e7bcf3166
Nicholas DaSilva <nmdas@umich.edu>
2026-02-27 21:55:54 -0500

    fixed hw bridge main not spining elevation node

--- hw_bridges/hw_bridge_main.cpp
+++ hw_bridges/hw_bridge_main.cpp
@@ -1,8 +1,10 @@
+#include "elevation_hw_bridge.cpp"
 #include "platform_hw_bridge.cpp"
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<PlatformHardwareBridge>());
+  rclcpp::spin(std::make_shared<ElevationHardwareBridge>());
   rclcpp::shutdown();
   return 0;
 }
