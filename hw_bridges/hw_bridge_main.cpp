2779e03f701c7209ffe61079f2ec161cb54d7238
NMDasilva05 <nmdas@umich.edu>
2026-04-09 18:21:05 -0400

    added drive hw bridge

--- hw_bridges/hw_bridge_main.cpp
+++ hw_bridges/hw_bridge_main.cpp
@@ -1,16 +1,19 @@
 #include "elevation_hw_bridge.cpp"
 #include "platform_hw_bridge.cpp"
+#include "drive_hw_bridge.cpp"
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
 
   auto platform_node = std::make_shared<PlatformHardwareBridge>();
   auto elevation_node = std::make_shared<ElevationHardwareBridge>();
+  auto drive_node = std::make_shared<DriveHardwareBridge>();
 
   rclcpp::executors::SingleThreadedExecutor executor;
 
   executor.add_node(platform_node);
   executor.add_node(elevation_node);
+  executor.add_node(drive_node);
 
   executor.spin();
 
